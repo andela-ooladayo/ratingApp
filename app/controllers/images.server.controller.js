d1b1932f879df5cc62759a1453f70281bed149a9
Oladayo Oyelade <ayfrank2@gmail.com>
2016-06-06 23:03:19 +0100

    CRUD for images

---
+++ app/controllers/images.server.controller.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var _ = require('lodash'),
+    db = require('../../config/sequelize'),
+    errorHandler = require('./errors');
+
+
+exports.create = function(req, res) {
+    var images = req.body;
+
+    db.images.create(req.body).done(function(err, images){
+        if(err){
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        }
+        return res.jsonp(images);
+    });
+};
+
+
+exports.read = function(req, res) {
+    res.jsonp(req.images);
+};
+
+
+exports.update = function(req, res) {
+
+    var images = req.images;
+
+    images = _.extend(images, req.body);
+
+    images.save().done(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(images);
+        }
+    });
+};
+
+
+exports.delete = function(req, res) {
+    var images = req.images;
+
+    images.destroy().done(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(images);
+        }
+    });
+};
+
+
+exports.listbyServiceId = function(req, res) {
+    var serviceId = req.body.serviceId;
+
+    db.images.findAll({where: { service_id: serviceId } }).done(function(err, images) {
+        if (err) {
+            return res.status(400).json(err);
+        }
+        else {
+            return res.status(200).json(images);
+        }
+    });
+};
+
+
+exports.imageByID = function(req, res, next, id) {
+    db.images.find({where: { id: id } }).done(function(err, images) {
+        if (err) return next(err);
+        if (!images) return next(new Error('Failed to load images ' + id));
+        req.images = images;
+        next();
+    });
+};
---
+++ app/routes/images.server.routes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var users = require('../../app/controllers/users'),
+    images = require('../../app/controllers/images');
+
+module.exports = function(app) {
+    app.route('/api/images')
+        .post(users.isAuthenticated, users.isAuthorized('merchant'), images.create);
+
+    app.route('/api/images/:imageId')
+        .get(images.read)
+        .put(users.isAuthenticated, users.isAuthorized('merchant'), images.update)
+        .delete(users.isAuthenticated, users.isAuthorized('merchant'), images.delete);
+
+    app.route('/api/images/fetch')
+        .post(users.isAuthenticated, images.listbyServiceId);
+
+    app.param('imageId', images.imageByID);
+};
