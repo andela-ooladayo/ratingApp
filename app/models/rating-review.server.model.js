8146a4956e093858e48722820e05d778d46b1749
Oladayo Oyelade <ayfrank2@gmail.com>
2016-06-09 22:35:19 +0100

    setup review/ratings components

---
+++ app/models/rating-review.server.model.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+    var ReviewRatings = sequelize.define('review_ratings', {
+        created: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        },
+        service_id: {
+            type: DataTypes.INTEGER
+
+        },
+        value: {
+            type: DataTypes.FLOAT
+        },
+        user_id: {
+            type: DataTypes.INTEGER
+        },
+        review: {
+            type: DataTypes.TEXT,
+            defaultValue: ''
+        },
+        no_of_likes: {
+            type: DataTypes.INTEGER
+        },
+        no_of_dislikes: {
+            type: DataTypes.INTEGER
+        }
+    });
+    return ReviewRatings;
+};
