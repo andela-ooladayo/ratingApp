08bd1a97367e24f427295952ce14b26842c49eb0
Oladayo Oyelade <ayfrank2@gmail.com>
2016-06-09 22:34:57 +0100

    lindelin oauth

---
+++ app/controllers/review-ratings.server.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _ = require('lodash'),
+    db = require('../../config/sequelize'),
+    errorHandler = require('./errors');
+
+
+exports.create = function(req, res) {
+    
+};
