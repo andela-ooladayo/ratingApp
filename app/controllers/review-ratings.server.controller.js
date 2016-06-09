d6cd69561a22bad43e44200a359ecbb445745d9a
Oladayo Oyelade <ayfrank2@gmail.com>
2016-06-09 22:46:23 +0100

    create method for review-ratings

--- app/controllers/review-ratings.server.controller.js
+++ app/controllers/review-ratings.server.controller.js
@@ -2,9 +2,19 @@
 
 var _ = require('lodash'),
     db = require('../../config/sequelize'),
+    async = require('async'),
     errorHandler = require('./errors');
 
 
 exports.create = function(req, res) {
-    
+    var reviewRating = req.body;
+
+    db.review_ratings.create(reviewRating).done(function(err, reviewRating){
+        if(err){
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        }
+        return res.staus(200).json({message: "sucessful"});
+    });
 };
