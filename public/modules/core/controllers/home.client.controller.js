8bdc2c5286aabbc063b17ebcb258b7c9fd3b9a62
dejijaye <dejijaye@gmail.com>
2016-06-24 13:38:02 +0100

    add rating block

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,6 +5,40 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+    $scope.view_tab = "home";
+    $scope.categories = [
+      "Agriculture & Agro-Allied",
+      "Banking & Finance (banks)",
+      "Business Services",
+      "ICT",
+      "Educational",
+      "Events & Entertainment",
+      "Nightlife and clubs",
+      "Fashion & Style",
+      "Food & Beverages",
+      "Maintenance Services",
+      "Manufacturing & Production",
+      "Medicine & Health",
+      "Pharmaceutical companies",
+      "Newspapers & Media",
+      "Oil & Gas",
+      "People & Society",
+      "Professional Services",
+      "Construction/Real Estate",
+      "Religious Organisations",
+      "Telecommunications",
+      "Travel & Tourism",
+      "Hotels",
+      "Restaurants",
+      "Industrial Goods",
+      "Consumer Goods",
+      "Services",
+      "Utilities",
+    ];
+    
+    $scope.changeTab = function(tab) {
+      $scope.view_tab = tab;
+    }
     $("#top").backstretch("http://dl.dropbox.com/u/515046/www/garfield-interior.jpg");
 	}
 ]);
