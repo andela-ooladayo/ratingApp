16f4be48a7bdd37713ddbc81136b32f98b814fb9
Oladayo Oyelade <ayfrank2@gmail.com>
2016-06-29 23:47:14 +0100

    fix bug

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -1,11 +1,15 @@
 'use strict';
 
 var users = require('../../app/controllers/users'),
+    core = require('../../app/controllers/core'),
     fileSigning = require('../../app/controllers/file.signing.server.controller');
 
 module.exports = function(app) {
+
+    app.route('*').get(core.index);
+
     app.route('/api/sign_s3')
-        .post(users.isAuthenticated, users.isAuthorized('user'), fileSigning.sign);
+        .get(users.isAuthenticated, users.isAuthorized('user'), fileSigning.sign);
 };
 
 
