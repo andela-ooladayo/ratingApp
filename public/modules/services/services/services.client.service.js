f038a74625e97648402b7426be138ece92687ae2
dejijaye <dejijaye@gmail.com>
2016-07-04 17:40:10 +0100

    fix serivce 404

--- public/modules/services/services/services.client.service.js
+++ public/modules/services/services/services.client.service.js
@@ -3,7 +3,7 @@
 //Services service used for communicating with the services REST endpoints
 angular.module('services').factory('Services', ['$resource',
     function($resource) {
-        return $resource('api/services/:serviceId', {
+        return $resource('api/service/:serviceId', {
             serviceId: '@id'
         }, {
             update: {
