2ec8639e55783b69ca9375842e0b9125b549ee69
dejijaye <dejijaye@gmail.com>
2016-07-16 14:42:43 +0100

    refactor rating mechanism

--- public/modules/services/controllers/services.client.controller.js
+++ public/modules/services/controllers/services.client.controller.js
@@ -94,7 +94,7 @@ angular.module('services').controller('ServicesController', ['$scope', '$statePa
 
         $scope.rating = 0;
         $scope.rateFunction = function(rating) {
-            // alert('Rating selected - ' + rating);
+            $scope.rating = rating;
         };
 
         $scope.isReadonly = false;
@@ -238,45 +238,40 @@ angular.module('services').controller('ServicesController', ['$scope', '$statePa
 .directive('starRating',
     function() {
         return {
-            restrict : 'A',
-            template : '<ul class="rating">'
-                     + '    <li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">'
-                     + '\u2605'
-                     + '</li>'
-                     + '</ul>',
-            scope : {
-                ratingValue : '=',
-                max : '=',
-                onRatingSelected : '&',
-                readonly: '=?'
+            restrict: 'A',
+            template: '<ul class="rating">' +
+            '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+            '\u2605' +
+            '</li>' +
+            '</ul>',
+            scope: {
+                ratingValue: '=',
+                max: '=',
+                readonly: '@',
+                onRatingSelected: '&'
             },
-            link : function(scope, elem, attrs) {
+            link: function(scope, elem, attrs) {
+
                 var updateStars = function() {
                     scope.stars = [];
-                    for ( var i = 0; i < scope.max; i++) {
-                        scope.stars.push({
-                            filled : i < scope.ratingValue
-                        });
+                    for (var i = 0; i < scope.max; i++) {
+                        scope.stars.push({ filled: i < scope.ratingValue });
                     }
                 };
-                
-                scope.toggle = function(index) {
-                    if (scope.readonly == undefined || scope.readonly === false) {
 
-                        scope.ratingValue = index + 1;
-                        scope.onRatingSelected({
-                            rating : index + 1
-                        });
+                scope.toggle = function(index) {
+                    if (scope.readonly && scope.readonly === 'true') {
+                        return;
                     }
+                    scope.ratingValue = index + 1;
+                    scope.onRatingSelected({ rating: index + 1 });
                 };
-                
-                scope.$watch('ratingValue',
-                    function(oldVal, newVal) {
-                        if (newVal  || newVal == 0) {
-                            updateStars();
-                        }
+
+                scope.$watch('ratingValue', function(oldVal, newVal) {
+                    if (newVal || newVal == 0) {
+                        updateStars();
                     }
-                );
+                });
             }
         };
     }
