e47a90e0344b9a1c45ca8cd081085ffb223b8fef
Oladayo Oyelade <ayfrank2@gmail.com>
2016-08-24 21:33:16 +0100

    minor fix

--- app/controllers/email.server.controller.js
+++ app/controllers/email.server.controller.js
@@ -1,5 +1,5 @@
-var api_key = 'key-56d8fcf90a99fa73da7494aa0f7b0b39';
-    var domain = 'sandboxf405e13ea86f4f47b3087ec341a42786.mailgun.org';
+var api_key = process.env.RAYTEE_MAILGUN_KEY || 'key-56d8fcf90a99fa73da7494aa0f7b0b39';
+    var domain = process.env.RAYTEE_MAILGUN_DOMAIN || 'sandboxf405e13ea86f4f47b3087ec341a42786.mailgun.org';
     var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 module.exports = function(msg, cb) {
